958710b9db8e66067d6052f6554354ec6147a07b
Shutov Dmitrii <fplatoff@gmail.com>
2023-11-21 14:21:05 +0400

    fix typo

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -2,6 +2,6 @@ from aiogram import types
 from loader import dp
 
 
-@dp.message_handler(commands=["ip"])
+@dp.message_handler(commands=["id"])
 async def cmd_start(message: types.Message):
     await message.answer(f"{message.from_id}")
